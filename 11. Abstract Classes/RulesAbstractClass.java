9a12370f1529a1fb9bfc8dd1a33d05f56041455c
Gayanand <gayanand.patel@gmail.com>
2025-06-06 06:05:41 +0530

    add complete files of Abstract classes demo

---
+++ 11. Abstract Classes/RulesAbstractClass.java
@@ -0,0 +1,23 @@
+abstract class Super
+{
+    abstract public void meth1();
+}
+
+class Sub extends Super
+{
+    public void meth1()
+    {
+        
+    }
+}
+
+public class RulesAbstractClass {
+
+    public static void main(String[] args) {
+        
+        // Super s;
+        
+        
+    }
+    
+}
