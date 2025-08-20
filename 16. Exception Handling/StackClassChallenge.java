126259bb283285a4e2bf10ba0119e98a3404fa35
Gayanand <gayanand.patel@gmail.com>
2025-08-21 00:27:05 +0530

    add complete demo programs of Exception Handling section

---
+++ 16. Exception Handling/StackClassChallenge.java
@@ -0,0 +1,81 @@
+class StackOverFlowException extends Exception
+{
+    public String toString()
+    {
+        return "Stack is Full";
+    }
+}
+
+class StackUnderFlowException extends Exception
+{
+    public String toString()
+    {
+        return "Stack is Empty";
+    }
+}
+
+
+class Stack
+{
+    private int size;
+    private int top=-1;
+    private int S[];
+    
+    public Stack(int sz)
+    {
+        size=sz;
+        S=new int[sz];
+    }
+    
+    public void push(int x) throws StackOverFlowException
+    {
+        if(top==size-1)
+            throw new StackOverFlowException();
+        top++;
+        S[top]=x;
+    }
+    
+    public int pop() throws StackUnderFlowException
+    {
+        int x=-1;
+        
+        if(top==-1)
+            throw new StackUnderFlowException();
+        x=S[top];
+        top--;
+        return x;              
+    }
+}
+
+
+
+
+public class StackClassChallenge
+{
+    public static void main(String[] args) 
+    {
+        Stack st=new Stack(5);
+        // try
+        // {
+        // st.push(10);
+        // st.push(15);
+        // st.push(10);
+        // st.push(15);
+        // st.push(10);
+        // st.push(15);
+        
+        // }
+        // catch(StackOverFlowException s)
+        // {
+        //     System.out.println(s);
+        // }
+        try{
+            st.pop();
+        }
+        catch(StackUnderFlowException s)
+        {
+            System.out.println(s);
+        }
+        
+    }   
+}
